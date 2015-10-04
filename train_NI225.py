b6ca5d84bad6914426e9490b2aaec47fe7a9d8aa
mana8626 <ginnsama@gmail.com>
2015-10-04 13:41:52 +0900

    testにもちゃんとデータ食わせた。

--- train_NI225.py
+++ train_NI225.py
@@ -16,49 +16,66 @@ import datetime
 # x_train
 # x_test
 
+# pandasでYahoo!ファイナンスから過去の日経平均株価を取得
 start = datetime.datetime(2014, 1, 1)
 end = datetime.datetime(2014, 9, 1)
 nikkei_data= web.DataReader('^N225', 'yahoo', start, end)
 
+# 今回欲しいのは始値だけなので切り出す
 values = nikkei_data.values
 target_data = []
 for i in range(0, len(values)):
     target_data.append(values[i][0])
 
-
+# 30日文のデータからその直後の5日間を予測させたいので、それぞれ切り出す。
 x_train = []
 for j in range(0, (len(values)-35)):
     x_train.append(target_data[j:j+30])
-#print(x_train)
 
 t_train = []
 for k in range(30, len(values)-5):
     t_train.append(target_data[k:k+5])
-#print(t_train)
 
-x_test = x_train
-t_test = t_train
+
+start = datetime.datetime(2015, 1, 1)
+end = datetime.datetime(2015, 9, 1)
+nikkei_data= web.DataReader('^N225', 'yahoo', start, end)
+
+values = nikkei_data.values
+target_data = []
+for l in range(0, len(values)):
+    target_data.append(values[l][0])
+
+x_test = []
+for m in range(0, (len(values)-35)):
+    x_test.append(target_data[m:m+30])
+
+t_test = []
+for n in range(30, len(values)-5):
+    t_test.append(target_data[n:n+5])
+
 
 x_train = np.array(x_train, dtype=np.float32)
 t_train = np.array(t_train, dtype=np.float32)
 x_test = np.array(x_test, dtype=np.float32)
 t_test = np.array(t_test, dtype=np.float32)
 
-
 n_train_batchset = len(x_train)
-
 n_test_batchset = len(x_test)
 
-
+print(n_train_batchset)
+print(n_train_batchset)
 
 # define constance
 n_input = 30
 n_output = 5
 n_units = 80
 
-n_epoch = 50
+n_epoch = 30
 
 batchsize = 2
+
+x_array = range(1, n_epoch+1)
 
 
 # define model
@@ -78,7 +95,8 @@ def forward(x_data, t_data, train = True):
 optimizer = optimizers.Adam()
 optimizer.setup(model)
 
-y_array = []
+train_loss = []
+test_loss = []
 
 # training loop
 for epoch in range(1, n_epoch + 1):
@@ -100,7 +118,7 @@ for epoch in range(1, n_epoch + 1):
 
     print('train mean loss={}'.format(sum_loss / n_train_batchset))
 
-    y_array.append(sum_loss / n_train_batchset)
+    train_loss.append(sum_loss / n_train_batchset)
 
     # evaluation
     sum_loss = 0
@@ -114,10 +132,11 @@ for epoch in range(1, n_epoch + 1):
 
     print('test  mean loss={}'.format(sum_loss / n_test_batchset))
 
+    test_loss.append(sum_loss / n_test_batchset)
+
 # do that you want to after training
 
-
-
-#x_array = range(1, 51)
-#plt.plot(x_array, y_array)
-#plt.show()
+plt.plot(x_array, train_loss, label="train_loss")
+plt.plot(x_array, test_loss, label="test_loss")
+plt.legend()
+plt.show()
